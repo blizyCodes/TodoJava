98da0376c4205b051f82b94b5c4da924b8283b31
blizyCodes <95969152+blizyCodes@users.noreply.github.com>
2025-06-16 15:04:53 +0100

    initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/nondas/todojava/TodoJavaApplication.java
@@ -0,0 +1,14 @@
+package com.nondas.todojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJavaApplication.class,
+							  args);
+	}
+
+}
---
+++ src/test/java/com/nondas/todojava/TodoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nondas.todojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
